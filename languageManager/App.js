f4924b3924f4fec8beb6a20edd637fd9470b0978
uffoltzl <61827376+uffoltzl@users.noreply.github.com>
2020-11-22 11:41:30 +0100

    create React Native App

---
+++ languageManager/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function App() {
+  const [name, setName] = React.useState("hello");
+  return (
+    <View style={styles.container}>
+      <TextInput 
+        value={name}
+        onChange={name => setName(name)}
+        editable
+      />
+      <Text>{name}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
